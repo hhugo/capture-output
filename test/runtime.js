a94afd3ed017f250a424fea0a7c9c375f3b60918
Hugo Heuzard <hugo.heuzard@gmail.com>
2026-03-31 10:28:21 +0200

    fixup! Add wasm_of_ocaml support
    fixup! Add wasm_of_ocaml support
    
    Simplify test JS runtime: use init_channels to store stdout/stderr
    channel values directly instead of looking up caml_sys_fds.

--- test/runtime.js
+++ test/runtime.js
@@ -1,24 +1,24 @@
 //Provides: capture_output_init_channels
-function capture_output_init_channels(stdout, stderr) { return 0; }
-
-//Provides: capture_output_log
-//Requires: caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes,caml_sys_fds
-function capture_output_log(fd, x) {
-    var chanid = caml_sys_fds[fd].chanid;
-    var s = caml_string_of_jsbytes(x);
-    caml_ml_output(chanid, s, 0, caml_ml_string_length(s));
-  }
+//Requires: caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
+var capture_output_stdout, capture_output_stderr;
+function capture_output_init_channels(stdout, stderr) {
+  capture_output_stdout = stdout;
+  capture_output_stderr = stderr;
+  return 0;
+}
 
 //Provides: capture_output_print_stdout
-//Requires: capture_output_log
+//Requires: capture_output_init_channels,caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
 function capture_output_print_stdout(unit) {
-  capture_output_log(1, "stdout from external");
+  var s = caml_string_of_jsbytes("stdout from external");
+  caml_ml_output(capture_output_stdout, s, 0, caml_ml_string_length(s));
   return 0
 }
 
 //Provides: capture_output_print_stderr
-//Requires: capture_output_log
+//Requires: capture_output_init_channels,caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
 function capture_output_print_stderr(unit) {
-  capture_output_log(2, "stderr from external");
+  var s = caml_string_of_jsbytes("stderr from external");
+  caml_ml_output(capture_output_stderr, s, 0, caml_ml_string_length(s));
   return 0
 }
