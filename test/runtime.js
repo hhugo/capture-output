568e1a7c74f479650c1b7e150fd70a0ca5ea684b
Hugo Heuzard <hugo.heuzard@gmail.com>
2026-03-31 10:42:11 +0200

    Fix test JS runtime free variable warnings and add missing wasm dep
    Declare capture_output_stdout/stderr as separate //Provides entries
    so js_of_ocaml doesn't report them as free variables. Add
    wasm_of_ocaml-compiler as a with-test dependency.

--- test/runtime.js
+++ test/runtime.js
@@ -1,6 +1,11 @@
+//Provides: capture_output_stdout
+var capture_output_stdout;
+
+//Provides: capture_output_stderr
+var capture_output_stderr;
+
 //Provides: capture_output_init_channels
-//Requires: caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
-var capture_output_stdout, capture_output_stderr;
+//Requires: capture_output_stdout, capture_output_stderr
 function capture_output_init_channels(stdout, stderr) {
   capture_output_stdout = stdout;
   capture_output_stderr = stderr;
@@ -8,7 +13,7 @@ function capture_output_init_channels(stdout, stderr) {
 }
 
 //Provides: capture_output_print_stdout
-//Requires: capture_output_init_channels,caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
+//Requires: capture_output_stdout,caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
 function capture_output_print_stdout(unit) {
   var s = caml_string_of_jsbytes("stdout from external");
   caml_ml_output(capture_output_stdout, s, 0, caml_ml_string_length(s));
@@ -16,7 +21,7 @@ function capture_output_print_stdout(unit) {
 }
 
 //Provides: capture_output_print_stderr
-//Requires: capture_output_init_channels,caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
+//Requires: capture_output_stderr,caml_ml_output,caml_ml_string_length,caml_string_of_jsbytes
 function capture_output_print_stderr(unit) {
   var s = caml_string_of_jsbytes("stderr from external");
   caml_ml_output(capture_output_stderr, s, 0, caml_ml_string_length(s));
